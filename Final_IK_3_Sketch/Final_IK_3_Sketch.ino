7c290ce8369dcbfc550cfbb4427ed251525eb313
mahesh <mahesh.kadam0802@gmail.com>
2024-05-12 19:58:44 +0530

    3D

--- Final_IK_3_Sketch/Final_IK_3_Sketch.ino
+++ Final_IK_3_Sketch/Final_IK_3_Sketch.ino
@@ -3,20 +3,35 @@
 #define SHOULDER 5.0
 #define ELBOW 5.0
 
-float ws_coord[2];
+float ws_coord[3];
 float d = 0;
+float angleA = 0;
 float angleC = 0;
 float angleB = 0;
+float angleD = 0;
 float b1 = 0;
 float b2 = 0;
-float z;
+
+Servo base;
 Servo shoulder;
 Servo elbow;
+Servo grip;
 
-int spin = 3;
-int epin = 5;
+int bpin = 3;
+int spin = 5;
+int epin = 6;
+int gpin = 9;
 
 #define PI 3.14159265
+
+void resetServo(){
+
+  base.write(90);
+  shoulder.write(90);
+  elbow.write(180);
+  grip.write(30);
+}
+
 void calcIK()
 {
   
@@ -27,7 +42,7 @@ void calcIK()
   }
 
   // Read all four angles from serial input
-  for (int i = 0; i < 2; ++i) {
+  for (int i = 0; i < 3; ++i) {
     ws_coord[i] = Serial.parseInt();
     // Skip over any non-numeric characters
     while (Serial.peek() == ' ' || Serial.peek() == '\n' || Serial.peek() == '\r') {
@@ -42,6 +57,7 @@ void calcIK()
   b1 = atan(ws_coord[1]/ws_coord[0])*(180/PI);
   b2 = (180-angleC)/2;
   angleB = b1+b2;
+  angleA = atan(ws_coord[2]/ws_coord[0])*(PI/180);
 }
 
 void setup()
@@ -49,11 +65,12 @@ void setup()
   Serial.begin(9600);
 //  elbow.setPeriodHertz(50);
 //  shoulder.setPeriodHertz(50);    
-  elbow.attach(epin);
+  base.attach(bpin);
   shoulder.attach(spin);
+  elbow.attach(epin);
+  grip.attach(gpin);
 
-  elbow.write(90);
-  shoulder.write(90);
+  resetServo();
 }
 
 void loop()
@@ -62,10 +79,21 @@ void loop()
   //z = atan(2)*(180/PI);
 //  Serial.println(z);
 //  delay(10);
-  Serial.println(angleC);
+  Serial.print("Base angle: ");
+  Serial.println(angleA);
   delay(100);
+  Serial.print("Shoulder angle: ");
   Serial.println(angleB);
   delay(100);
+  Serial.print("elbow angle: ");
+  Serial.println(angleC);
+  delay(100);
+  Serial.print("Gripper angle: ");
+  Serial.println(angleD);
+  delay(100);
+
+  base.write(angleA);
   shoulder.write(angleB);
   elbow.write(angleC);
+  grip.write(angleD);
 }
